615b3b020d1ce96bfd79fd232e15fe9f17cebb7c
sachin <sachin.kumar@socwise.com>
2020-11-02 13:06:42 +0530

    convert into cli

--- screens/Events.js
+++ screens/Events.js
@@ -5,7 +5,7 @@ import {Agenda} from 'react-native-calendars';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 function Events(props) {
     const [data, setData] = useState({})
-    const {dateString,day,month,timestamp,year}=props.route.params.day;
+    const {dateString}=props.route.params.day;
     const {markedData}=props.route.params
 
     useEffect(() => {
@@ -14,14 +14,24 @@ function Events(props) {
             obj[dateString]=JSON.parse(v)
             setData(obj)
         })
+        setData(dateString)
     }, [])
     const openModel=(item)=>{
         props.navigation.navigate("Task",{task:item})
     }
+    const selectDate=async(dateString)=>{
+        AsyncStorage.getItem(dateString).then(v=>{
+            const obj={}
+            obj[dateString]=JSON.parse(v)
+            setData(obj)
+        })
+        setData(dateString)
+   }
     return (
         <Agenda
             items={data}
             selected={dateString}
+            onDayPress={(day) => selectDate(day.dateString)}
             renderItem={(item, firstItemInDay) => {return (<View><TouchableOpacity onPress={()=>{
                 openModel(item);
             }}><Text>{item.name}</Text></TouchableOpacity></View>);}}
